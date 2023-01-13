b19c8996f5fe071d5e0b8cf97bc4091a12d6cfc7
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-01-13 18:11:05 +0900

    var keyword

--- bin/01.variable/b.var_keyword.dart
+++ bin/01.variable/b.var_keyword.dart
@@ -1,1 +1,7 @@
-void main() {}
+void main() {
+  String name = '니꼬';
+  name = 'nico';
+  // var, String 모두 사용o
+  /** 함수, 메소드 내부에 지역변수 선언: var 
+   *  class에서 변수, property를 선언할 때 타입 지정: Stinrg, int...*/
+}
