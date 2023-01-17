f1587e5844c89e4db88fccc91267adde56456ded
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-01-17 23:08:35 +0900

    constant variables

---
+++ bin/01.variable/7.constant_var.dart
@@ -0,0 +1,6 @@
+void main() {
+  // dart의 final이 js, ts의 const와 같다.
+  // const는 컴파일 할 때 알고 있는 값을 넣는 것.
+  const API = 'nico';
+  // final API = fetchApi(); 변수에 들어오는 값을 모를경우 final, var을 사용하는 것이 나음.
+}
