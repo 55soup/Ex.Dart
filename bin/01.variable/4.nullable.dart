4e10f337ec735941656d67a25046cfebd710d485
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-01-17 13:23:06 +0900

    nullable type

---
+++ bin/01.variable/4.nullable.dart
@@ -0,0 +1,8 @@
+void main() {
+  // null safety: 어떤변수, 혹은 데이터가 null이 될 수 있음을 명시하는 것
+  String? nico = 'nico'; // ?: String일 수도 null일 수도
+  nico = null;
+  if (nico != null) {
+    nico.isNotEmpty;
+  }
+}
