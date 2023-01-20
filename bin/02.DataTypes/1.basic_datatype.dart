35fe72dbae203ca7e178375a4c35a00a561bab92
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-01-21 01:11:46 +0900

    2.0 Basic Data Types

---
+++ bin/02.DataTypes/1.basic_datatype.dart
@@ -0,0 +1,12 @@
+void main() {
+  // dart의 type들은 모두 class임.
+  // => dart는 객체지향언어.
+  String name = "nico";
+  bool alive = true;
+  int age = 12;
+  double money = 69.99;
+  // num 자료형을 사용하면 int/double임.
+  // num은 int, double의 부모 자료형임.
+  num x = 12;
+  x = 1.1;
+}
